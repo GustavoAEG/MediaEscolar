334c4f0b9e991e17b613b33fcc60ca1bb5945c16
Gustavo <gustavoenriccoga@gmail.com>
2021-11-04 19:50:56 -0300

    atualizando

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,59 @@
+import classe.Student
+
 fun main(args: Array<String>) {
 
-    mostrarNome()
+    val rosnei: Student = Student(
+        "Rosnei", "da Silva",
+        123, 50.0, 2
+    )
+
+    val maria: Student = Student(
+        "Maria", "da Silva",
+        1234, 80.0, 1
+    )
+
+    val henrique: Student = Student(
+        "Henrique", "da Silva Felipe",
+        12345, 10.0, 3
+    )
+
+    val rebeca: Student = Student(
+        "Rebeca", "da Silva",
+        123456, 70.0, 1
+    )
+
+    try{
+        banco.conexao(user, senha)
+        print("Agora digite a consulta que vc deseja: ")
+        val consulta = readLine()!!
+        banco.consulta(consulta)
+    }catch (e: Exception){
+        println(e.message)
+    }
+
+}
+
+    rosnei.nomeCompleto()
+    rosnei.mudarAnoSeAprovado()
+
+    val cursoIngles = Courses("Inglês", "Genivaldo",
+        1)
+
+    cursoIngles.cadastrar(rosnei)
+    cursoIngles.cadastrar(maria)
+
+    cursoIngles.numEstudantesCadastrados()
+
+    cursoIngles.removerEstudante(rebeca)
+
+    val estudantes: Array<Student?> = arrayOf(henrique, rebeca)
+
+    cursoIngles.cadastrar(estudantes)
+
+    cursoIngles.numEstudantesCadastrados()
+
+    cursoIngles.melhorNota()
+
+    cursoIngles.mostrarEstudantes()
 
 }
